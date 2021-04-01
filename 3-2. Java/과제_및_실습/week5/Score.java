a93920ffbe86d1bda5ac6970c2027b0385a38e66
seungmin-park <78605779+seungmin-park@users.noreply.github.com>
2021-04-01 19:28:23 +0900

    exercise java

---
+++ 3-2. Java/과제_및_실습/week5/Score.java
@@ -0,0 +1,35 @@
+package 과제_및_실습.week5;
+
+import java.util.Scanner;
+
+public class Score {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        
+        System.out.println("    SCORE       GRADE");
+        System.out.println("========================");
+        while(run){
+            int scanData = sc.nextInt();
+            if(scanData <= 100 && scanData >= 90){
+                System.out.println("    "+scanData+"       A");
+            }else if(scanData >= 80){
+                System.out.println("    "+scanData+"       B");
+            }else if(scanData >= 70){
+                System.out.println("    "+scanData+"       C");
+            }else if(scanData >= 60){
+                System.out.println("    "+scanData+"       D");
+            }else if(scanData >= 50){
+                System.out.println("    "+scanData+"       E");
+            }else if(scanData >=0 && scanData <50){
+                System.out.println(scanData+"       F");
+            }
+
+            if(scanData <0){
+                run = false;
+            }
+        }
+        System.out.println("프로그램 종료");
+        sc.close();
+    }
+}
