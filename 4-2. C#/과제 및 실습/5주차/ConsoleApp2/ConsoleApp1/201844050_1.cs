e750e2fb35bfb13f42dcaa859a3d9d2194b7dca8
seungmin-park <78605779+seungmin-park@users.noreply.github.com>
2021-04-04 01:38:08 +0900

    C# report

---
+++ 4-2. C#/과제 및 실습/5주차/ConsoleApp2/ConsoleApp1/201844050_1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] scores = { 81.5, 71.0, 91 };
+            int[ , ] test = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+            foreach (double score in scores) { Console.WriteLine( score); }
+            Array.Sort(scores);
+            foreach (var score in scores) { Console.WriteLine(score); }
+
+            int rank_a = scores.Rank;
+            int rank_b = test.Rank;
+
+            Console.WriteLine("{0} / {1} ", rank_a, rank_b);
+
+            int idx_a = Array.IndexOf(scores, 80);
+            int idx_b= Array.IndexOf(scores, 91);
+            Console.WriteLine(string.Format("{0} / {1}", idx_a, idx_b));
+
+            Array.Clear(scores, 1, 2); //이거 찾아보기
+            foreach (var score in scores) { Console.WriteLine(score); }
+
+            var row = test.GetLength(0);
+            var col = test.GetLength(1);
+            Console.WriteLine($"{row} / {col}");
+
+            foreach(var t in test)
+            {
+                Console.WriteLine("{0} ", t);
+            }
+            Console.WriteLine();
+
+            for(int i = 0; i <test.GetLength(0); i++)
+            {
+                for(int j = 0; j <test.GetLength(1); j++)
+                {
+                    Console.WriteLine("{0} ", test[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 4-2. C#/과제 및 실습/5주차/ConsoleApp2/ConsoleApp2/201844050_2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+        public string CarNumber;
+        public DateTime InTime;
+        public DateTime OutTime;
+
+        public int ParkingTime()
+        {
+            return (int)(this.OutTime - this.InTime).TotalMinutes;
+        }
+
+        public string PrintState()
+        {
+            int parkingTime = ParkingTime();
+            string message;
+            if(parkingTime > 0)
+            {
+                message = string.Format("[{0}] {1}분 주차 ", this.CarNumber, parkingTime);
+            }
+            else
+            {
+                message = string.Format("[{0}] 주차중", this.CarNumber);
+            }
+
+            return message;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            // 코드 완성
+            Console.WriteLine(car.PrintState());
+        }
+    }
+}
