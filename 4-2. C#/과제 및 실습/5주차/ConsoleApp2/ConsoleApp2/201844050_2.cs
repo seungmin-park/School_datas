2bd320c1aa27a76defc8418a251798544098a3ca
seungmin-park <78605779+seungmin-park@users.noreply.github.com>
2021-04-04 18:34:12 +0900

    C# report ver1

--- 4-2. C#/과제 및 실습/5주차/ConsoleApp2/ConsoleApp2/201844050_2.cs
+++ 4-2. C#/과제 및 실습/5주차/ConsoleApp2/ConsoleApp2/201844050_2.cs
@@ -38,8 +38,34 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Car car = new Car();
-            // 코드 완성
-            Console.WriteLine(car.PrintState());
+            Console.Write("차량번호:");
+            string carNumber = Console.ReadLine();
+            if(string.IsNullOrEmpty(carNumber) == true)
+            {
+                Console.WriteLine("차량번호를 제대로 입력해주세요.");
+            }
+            else
+            {
+                car.CarNumber = carNumber;
+                Console.Write("입고시간 (예:2012/4/3 12:02:03) : ");
+                string inTimeput = Console.ReadLine();
+                if (string.IsNullOrEmpty(inTimeput) == true)
+                {
+                    Console.WriteLine("입고시간이 형식이 잘못되었습니다.");
+                }
+                else
+                {
+                    string[] test = inTimeput.Split(' ');
+                    string[] date = test[0].Split('/');
+                    string[] day = test[1].Split(':');
+                    Console.Write("출고시간 (예:2012/4/3 12:02:03) : ");
+                    string outTimeput = Console.ReadLine();
+                    if(string.IsNullOrEmpty(outTimeput) == true)
+                    {
+                        Console.WriteLine(car.PrintState());
+                    }
+                }
+            }
         }
     }
 }
