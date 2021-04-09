b4786cfe0e3e31ddb602acd99eb8f1c4fba7ecd5
seungmin-park <78605779+seungmin-park@users.noreply.github.com>
2021-04-10 01:20:58 +0900

    delete bin, obj, .vs and .vscode

--- 4-2. C#/과제 및 실습/6주차/week6/week6_3/Form1.cs
+++ 4-2. C#/과제 및 실습/6주차/week6/week6_3/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,28 +16,34 @@ namespace week6_3
         //인스턴스 변수
         List<Car> _cars = new List<Car>();//Car[] _cars = new Car[10]; int _currentCarCount = 0;
 
+        //생성자
         public Form1()
         {
             InitializeComponent();
         }
-
+        
+        //인스턴스 메소드
         private void btnIn_Click(object sender, EventArgs e)
         {
             string number = tbxNumber.Text.Trim();
-            if (true == string.IsNullOrWhiteSpace(number))
-            {
-                MessageBox.Show("차량 번호를 반드시 넣어주세요 . ");
-                return; //메소드 탈출문
-            }
 
-            foreach (Car car in _cars)
+            if (!CheckCarNumber(number))
             {
-                if (car.CarNumber == number)
-                {
-                    MessageBox.Show("현재 입고되어 있는 차량 번호입니다.");
-                    return;
-                }
+                MessageBox.Show("차량번호의 형식이 어긋났습니다.");
             }
+            if(SearhCar(number) != null)
+            {
+                MessageBox.Show("현재 입고되어 있는 차량 번호입니다.");
+                return;
+            }
+            //foreach (Car car in _cars)
+            //{
+            //    if (car.CarNumber == number)
+            //    {
+            //        MessageBox.Show("현재 입고되어 있는 차량 번호입니다.");
+            //        return;
+            //    }
+            //}
             //for (int i = 0; i < _cars.Count; i++)
             //{
             //    if (_cars[i].CarNumber == number)
@@ -47,7 +54,7 @@ namespace week6_3
             // }
             Car newCar = new Car();
             newCar.CarNumber = number;
-            newCar.InTime = DateTime.Now.AddMinutes(-47);
+            newCar.InTime = DateTime.Now.AddMinutes(MakeTestTime());
 
             _cars.Add(newCar);
 
@@ -58,34 +65,37 @@ namespace week6_3
         {
 
             string number = tbxNumber.Text.Trim();
-            if (true == string.IsNullOrWhiteSpace(number))
+            if (!CheckCarNumber(number))
             {
-                MessageBox.Show("차량 번호를 반드시 넣어주세요 . ");
-                return; //메소드 탈출문
+                MessageBox.Show("차량번호의 형식이 어긋났습니다.");
             }
 
-            foreach (var car in _cars)
+            var car = SearhCar(number);
+
+            if(car != null)
             {
-                if (car.CarNumber == number)
+                MessageBox.Show("현재 입고 되어있는 차량이 없습니다.");
+            }
+            else
+            {
+                car.OutTime = DateTime.Now;
+                int charge = car.CalcParkingCharge();
+                int min = car.ParkingTime();
+
+                if (charge == 0)
                 {
-                    car.OutTime = DateTime.Now;
-                    int charge = car.CalcParkingCharge();
-                    int min = car.ParkingTime();
-
-                    if (charge == 0)
-                    {
-                        MessageBox.Show($"회차 차량입니다. ({min})분");
-                    }
-                    else
-                    {
-                        MessageBox.Show(string.Format("{0}분 주차 / {1}원", min, charge));
-                    }
-
-                    this._cars.Remove(car);
-
-                    return;
+                    MessageBox.Show($"회차 차량입니다. ({min})분");
                 }
+                else
+                {
+                    MessageBox.Show(string.Format("{0}분 주차 / {1}원", min, charge));
+                }
+
+                this._cars.Remove(car);
+
+                return;
             }
+
 
             //for (int i = 0; i < _cars.Count; i++)
             //{
@@ -105,7 +115,7 @@ namespace week6_3
             //        return;
             //    }
             //}
-            MessageBox.Show("현재 입고 되어있는 차량이 없습니다.");
+            
         }
 
         private void btnView_Click(object sender, EventArgs e)
@@ -123,5 +133,30 @@ namespace week6_3
             }
             tbxView.Text = message;
         }
-    }
-}
+
+        private bool CheckCarNumber(string number)
+        {
+            Regex regx = new Regex(@"^[0-9]{2, 3}[가-힣]{1}[0-9]{4}$");
+            Match m = regx.Match(number);
+            return m.Success;
+        }
+
+        private int MakeTestTime()
+        {
+            Random rand = new Random();
+            return rand.Next(-70, 0);
+        }
+
+        private Car SearhCar(string number)
+        {
+            foreach(var car in _cars)
+            {
+                if(car.CarNumber == number)
+                {
+                    return car;
+                }
+            }
+            return null;
+        }
+    }//class
+}//namespace
