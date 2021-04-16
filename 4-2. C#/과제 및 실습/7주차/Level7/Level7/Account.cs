55a930c89485040382d9e6616b8080c24e9dc60a
seungmin-park <78605779+seungmin-park@users.noreply.github.com>
2021-04-16 22:53:19 +0900

    7th week C# and DB report

---
+++ 4-2. C#/과제 및 실습/7주차/Level7/Level7/Account.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level7
+{
+    class Account
+    {
+        private static int _maxBalance = 10000000;// static 클래스 변수
+        private string _owner;
+        public string Owner //프로퍼티
+        {
+            get
+            {
+                return this._owner;
+            }
+            set
+            {
+                this._owner = value;
+            }
+        }
+        
+        private string _number;
+        private int _balance;
+
+        //public Account() //기본 생성자
+        //{
+        //}
+
+        public Account(string number, string name, int money)//사용자 정의 생성자
+        {
+            this._number = number;
+            this._owner = name;
+            this._balance = money;
+        }
+
+        public Account(string number, string name)//사용자 정의 생성자
+        {
+            this._number = number;
+            this._owner = name;
+            //this._balance = 0; 인스턴스 변수는 기본적으로 초기화됨
+        }
+
+        public bool Deposit(int amount)//amount 는 지역변수이자 매개변수
+        {
+            if (this._balance + amount > Account._maxBalance)
+            {
+                return false;
+            }
+            else
+            {
+                this._balance += amount;
+                return true;
+            }
+        }
+
+        public bool WithDraw(int amount)
+        {
+            if(this._balance - amount < 0)
+            {
+                return false;
+            }
+            else
+            {
+                this._balance -= amount;
+                return true;
+            }
+        }
+
+        public StringBuilder Info()
+        {
+            StringBuilder builder = new StringBuilder();
+            //문자열을 여러개 조합할때 +연산자 대신 스트링 빌더 사용하기 + 연산자는 힙영역이 계속 증식
+            builder.Append("번호 : ").Append(this._number);
+            builder.Append(Environment.NewLine);
+            builder.Append("이름 : ").Append(this._owner);
+            builder.Append(Environment.NewLine);
+            builder.Append("잔액 : ").Append(this._balance);
+
+            return builder;
+        }
+    }
+}
---
+++ 4-2. C#/과제 및 실습/7주차/Level7/Level7/BankSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level7
+{
+    class BankSystem
+    {
+        static List<Account> accounts = null;//인스턴스 변수 클래스안 에서 서언 static 안붙음
+        static void Main(string[] args)
+        {
+
+            //Account acc1 = new Account();
+            Account acc2 = new Account("111-111-111", "김인하", 1000);
+            Account acc3 = new Account("211-111-111", "이인하");
+
+            acc2.Deposit(10000);//seter 역할
+            acc2.WithDraw(2000);
+            acc2.Owner = "박인하";
+            Console.WriteLine(acc2.Owner);
+            Console.WriteLine(acc2.Info());
+
+            //accounts = new list<account>();//클래스메소드는 인스턴스 변수에 접근할수없음
+
+            //account acc1 = new account();
+            //account acc2 = new account();
+
+            //acc1._number = "111-111";
+            //acc1._owner = "김인하";
+            //acc1._balance = 1000;
+
+            //accounts.add(acc1);
+            //accounts.add(acc2);
+
+            //accounts[0].withdraw(10000);//acc1의 잔액이 1000이라 10000을 빼는것이 불가능해 false
+            //acc1.deposit(10);
+            //acc2.deposit(200);
+            //accounts[1].withdraw(1);
+
+            //console.writeline(acc1._balance);
+            //console.writeline(acc2._balance);
+        }
+    }
+}
