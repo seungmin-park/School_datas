f917981ec7af307cc63a99d2df3c4323a057c315
seungmin-park <78605779+seungmin-park@users.noreply.github.com>
2021-05-13 17:02:50 +0900

    Update sketch_apr30b.ino

--- 4-1. 사물인터넷/과제 및 실습/9주차/sketch_apr30b/sketch_apr30b.ino
+++ 4-1. 사물인터넷/과제 및 실습/9주차/sketch_apr30b/sketch_apr30b.ino
@@ -10,14 +10,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   lcd.setCursor(0,0);
-  lcd.print("Welcome to");
-  lcd.setCursor(1,1);
-  lcd.print("ARDUINO world!");
-  delay(3000);
-  lcd.clear();
-  lcd.setCursor(0,0);
   lcd.print("201844050");
   lcd.setCursor(1,1);
   lcd.print("park seung min");
-  delay(3000);
 }
