7d02af2e09abffb03341072609103f990e05d82c
seungmin-park <tmddudals369@naver.com>
2021-09-16 03:29:19 +0900

    난수 추출된 수의 소수 판정 프로그램 v0.6

--- 2021-2/1-2. opensource/week3/syntax04/syntax04.go
+++ 2021-2/1-2. opensource/week3/syntax04/syntax04.go
@@ -5,20 +5,22 @@ import (
 	"math/rand"
 	"time"
 )
-//난수 추출된 수의 소수 판정 프로그램 v0.4
+//난수 추출된 수의 소수 판정 프로그램 v0.6
 func main() {
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 
 	isPrime := true
 	number := rand.Intn(150) + 2
+	//number = 21
 	fmt.Println("임의로 추출된 수 : ",number)
 
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
 			isPrime = false
+			break // 첫 번째 약수 발견되면 반복문 즉시 종료
 		}
-		fmt.Print(i, " ")
+		//fmt.Print(i, " ")
 	}
 
 	if isPrime { // 비교 연산자 제거
@@ -27,6 +29,36 @@ func main() {
 		fmt.Println(number,"는(은) 소수가 아닙니다!")
 	}
 }
+
+//package main
+//
+//import (
+//	"fmt"
+//	"math/rand"
+//	"time"
+//)
+////난수 추출된 수의 소수 판정 프로그램 v0.4
+//func main() {
+//	seed := time.Now().Unix()
+//	rand.Seed(seed)
+//
+//	isPrime := true
+//	number := rand.Intn(150) + 2
+//	fmt.Println("임의로 추출된 수 : ",number)
+//
+//	for i := 2; i < number; i++ {
+//		if number%i == 0 {
+//			isPrime = false
+//		}
+//		fmt.Print(i, " ")
+//	}
+//
+//	if isPrime { // 비교 연산자 제거
+//		fmt.Println(number,"는(은) 소수입니다!")
+//	}else {
+//		fmt.Println(number,"는(은) 소수가 아닙니다!")
+//	}
+//}
 
 //package main
 //
