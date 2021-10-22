c1d6df0dd84df06f6739b69bd3bf4aeedfc5e636
seungmin-park <tmddudals369@naver.com>
2021-10-22 17:13:13 +0900

    js 시간 수정

--- 2021-2/4-2. JSP/week8/target/week8-1.0-SNAPSHOT/js/realTime.js
+++ 2021-2/4-2. JSP/week8/target/week8-1.0-SNAPSHOT/js/realTime.js
@@ -3,12 +3,12 @@ var clock = document.querySelector('.viewCurrentTime');
 function getTime() {
     const time = new Date();
     const year = time.getFullYear();
-    const month = time.getMonth();
-    const day = time.getDay();
+    const month = time.getMonth()+1;
+    const date = time.getDate();
     const hour = time.getHours();
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
-    clock.innerHTML = `현재 접속 시각 : ${year}년 ${month}월 ${day}일 ${hour<10 ? `0${hour}`:hour}:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}`:seconds}`;
+    clock.innerHTML = `현재 접속 시각 : ${year}년 ${month}월 ${date}일 ${hour<10 ? `0${hour}`:hour}:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}`:seconds}`;
 }
 
 function init() {
